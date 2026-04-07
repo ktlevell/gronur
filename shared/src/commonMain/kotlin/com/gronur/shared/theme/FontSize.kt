a79d7fc98de814dba7ab87ed64c2b27ddb9b77c2
Jhônatan Santana <jhonatansantanati@gmail.com>
2026-04-06 22:02:55 -0300

    style: add font and create typography

---
+++ shared/src/commonMain/kotlin/com/gronur/shared/theme/FontSize.kt
@@ -0,0 +1,15 @@
+package com.gronur.shared.theme
+
+import androidx.compose.ui.unit.sp
+
+
+object FontSize {
+    val EXTRA_SMALL = 12.sp
+    val SMALL = 14.sp
+    val REGULAR = 16.sp
+    val EXTRA_REGULAR = 18.sp
+    val MEDIUM = 20.sp
+    val EXTRA_MEDIUM = 22.sp
+    val LARGE = 30.sp
+    val EXTRA_LARGE = 40.sp
+}
