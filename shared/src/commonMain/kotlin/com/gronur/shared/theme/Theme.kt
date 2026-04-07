af44ed479ac14b803fc225c7a3055e28429c1a97
Jhônatan Santana <jhonatansantanati@gmail.com>
2026-04-06 22:41:13 -0300

    style: add theme

--- shared/src/commonMain/kotlin/com/gronur/shared/theme/Theme.kt
+++ shared/src/commonMain/kotlin/com/gronur/shared/theme/Theme.kt
@@ -1,2 +1,17 @@
 package com.gronur.shared.theme
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GronurTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
