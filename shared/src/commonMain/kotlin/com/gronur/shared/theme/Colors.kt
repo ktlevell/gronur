7e8279938fa4482c7a31265605c7b1e988076166
Jhônatan Santana <jhonatansantanati@gmail.com>
2026-04-06 22:42:36 -0300

    style: add theme

--- shared/src/commonMain/kotlin/com/gronur/shared/theme/Colors.kt
+++ shared/src/commonMain/kotlin/com/gronur/shared/theme/Colors.kt
@@ -1,5 +1,6 @@
 package com.gronur.shared.theme
 
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 /**
@@ -37,3 +38,20 @@ val Tertiary0100 = Color(0xFFF6AC469)
 val Tertiary075 = Color(0xFFFFA4DBA3)
 val Tertiary50 = Color(0xFFFC8E9C8)
 val Tertiary025 = Color(0xFFFFEDF8ED)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF010101)
+
+
+
+val LightColorScheme = lightColorScheme(
+    primary = Primary0100,
+    onPrimary = White,
+    onPrimaryFixedVariant = Black,
+    secondary = Secondary0100,
+    onSecondary = White,
+    surface = BG3,
+    onSurface = BG1,
+    onSurfaceVariant = BG2,
+    tertiary = Tertiary0100,
+)
